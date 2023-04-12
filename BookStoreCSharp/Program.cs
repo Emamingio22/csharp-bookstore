f989a4e5af5d964663f31f471996b4c763f6b8e7
emamingio22 <emawankenoby02@gmail.com>
2023-04-12 15:27:25 +0200

    informazioni amazon

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -15,5 +15,23 @@ float larghezza = 17.78f;
 float lunghezza = 23.37f;
 float profondità = 2.79f;
 
+Console.WriteLine();
+Console.WriteLine("informazioni amazon: ");
+Console.WriteLine();
+int recensioni = 5413;
+float valutazioneMedia = 4.7f;
+bool iskindle = true;
+bool isflessibile = true;
+string Kindle = "";
+string flessibile = "";
+
+
+if (iskindle)
+{
+    kindle = "SI";
+} else
+{
+    kindle = "NO";
+}
 
 
