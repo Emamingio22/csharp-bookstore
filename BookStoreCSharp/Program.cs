12b1669f6af2217375e5c78ee0b1690f2adfe2d0
emamingio22 <emawankenoby02@gmail.com>
2023-04-12 15:08:48 +0200

    title and author

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine();
 
 
+string title = "Clean Code: A Handbook of Agile Software Craftsmanship";
+string author = "Robert C. Martin";
+Console.WriteLine();
+Console.WriteLine("informazioni generiche: ");
+
