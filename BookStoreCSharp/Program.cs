590cee60f1d46f5d713e566774dbf659071c4ca9
emamingio22 <emawankenoby02@gmail.com>
2023-04-12 15:39:51 +0200

    informazioni generali stampate

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -8,30 +8,54 @@ Console.WriteLine();
 Console.WriteLine("informazioni generiche: ");
 Console.WriteLine();
 long ISBN10 = 9780132350884;
-long ISBN13 = 978 - 0132350884;
 int numeroPagine = 431;
 float pesoLibro = 0.800f;
 float larghezza = 17.78f;
 float lunghezza = 23.37f;
 float profondità = 2.79f;
 
+
+Console.WriteLine("TITOLO: " + title);
+Console.WriteLine("AUTORE: " + author);
+Console.WriteLine("ISBN-10: " + ISBN10);
+Console.WriteLine("NUMERO PAGINE: " + numeroPagine + " pagine");
+Console.WriteLine("PESO ARTICOLO: " + pesoLibro);
+Console.WriteLine("DIMENSIONI: " + larghezza + " x " + profondità + " x " + lunghezza + " cm");
+
+
+
+
+
+
+
 Console.WriteLine();
 Console.WriteLine("informazioni amazon: ");
 Console.WriteLine();
 int recensioni = 5413;
 float valutazioneMedia = 4.7f;
-bool iskindle = true;
-bool isflessibile = true;
-string Kindle = "";
+bool isKindle = true;
+bool isFlessibile = true;
+string kindle = "";
 string flessibile = "";
 
 
-if (iskindle)
+if (isKindle)
 {
     kindle = "SI";
 } else
 {
     kindle = "NO";
 }
+
+
+if (isFlessibile)
+{
+    flessibile = "SI";
+}
+else
+{
+    flessibile = "NO";
+}
+
 
 
