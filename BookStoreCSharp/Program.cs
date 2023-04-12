60adf3f374fb518cf1f409d161b90a618b0fe5c9
emamingio22 <emawankenoby02@gmail.com>
2023-04-12 15:18:52 +0200

    informazioni generiche

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -6,4 +6,14 @@ string title = "Clean Code: A Handbook of Agile Software Craftsmanship";
 string author = "Robert C. Martin";
 Console.WriteLine();
 Console.WriteLine("informazioni generiche: ");
+Console.WriteLine();
+long ISBN10 = 9780132350884;
+long ISBN13 = 978 - 0132350884;
+int numeroPagine = 431;
+float pesoLibro = 0.800f;
+float larghezza = 17.78f;
+float lunghezza = 23.37f;
+float profondità = 2.79f;
+
+
 
